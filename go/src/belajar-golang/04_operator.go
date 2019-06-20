db5402225c942a6971eb1abf869b68e5fe61f0e4
Dilarida Alrizeki <51981943+dilaalrizeki@users.noreply.github.com>
2019-06-20 08:11:54 +0700

    Update 04_operator.go

--- go/src/belajar-golang/04_operator.go
+++ go/src/belajar-golang/04_operator.go
@@ -2,6 +2,15 @@ package main
 import "fmt"
 
 func main() {
-	var nilai = (3*6%3)
+	// Ada 3 Jenis operator = Aritmatika, Perbandingan dan Logika
+	// #1 Aritmatika ( + - * / % )
+	var nilai = (((6 + 6) % 3) + 11 - 2) / 3
 	fmt.Println(nilai)
+	// #2 Perbandingan (==, !=, <, >, <=, >=
+	var isEqual = (nilai == 2)
+	fmt.Println("apakah",nilai, "== 2 : ", isEqual)
+	// #3 Logika (||, &&, !)
+	a,b := true, false
+	operatorDan := a && b
+	fmt.Printf("a && b = (%t)  \n", operatorDan)
 }
